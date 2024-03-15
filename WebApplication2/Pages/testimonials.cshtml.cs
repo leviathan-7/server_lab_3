9211b9fc572760e2a3d02995e62c11de06df3bb0
leviathan-7 <m.grober02@yandex.ru>
2024-03-15 20:04:28 +0300

    add information from form to BD

--- WebApplication2/Pages/testimonials.cshtml.cs
+++ WebApplication2/Pages/testimonials.cshtml.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication2.Data;
@@ -12,6 +16,7 @@ using WebApplication2.Models;
 
 namespace WebApplication2.Pages
 {
+    [IgnoreAntiforgeryToken]
     public class TestimonialsModel : PageModel
     {
         private readonly WebContext _context;
@@ -23,6 +28,14 @@ namespace WebApplication2.Pages
         public void OnGet()
         {
             Testimonials = _context.Testimonials.AsNoTracking().ToList();
+        }
+        public record Inf(string name, string title, string ocupation, string description);
+
+        public IActionResult OnPost(Inf inf)
+        {
+            _context.Testimonials.Add(new Testimonial { name = inf.name, img_irl = "avatar.jpg", ocupation = inf.ocupation, title = inf.title, description = inf.description });
+            _context.SaveChanges();
+            return Content("<fieldset><div id='success_page'><h1>Email Sent Successfully.</h1><p>Thank you <strong>" + inf.name + "</strong></p></div></fieldset>");
         }
     }
 }
--- WebApplication2/wwwroot/js/custom.js
+++ WebApplication2/wwwroot/js/custom.js
@@ -132,7 +132,38 @@
             });
             return false;
         });
+
     });
+
+    jQuery(document).ready(function () {
+    $('#testimonialform').submit(function () {
+        var action = $(this).attr('action');
+        $("#message").slideUp(750, function () {
+            $('#message').hide();
+            $('#submit')
+                .after('<img src="images/ajax-loader.gif" class="loader" />')
+                .attr('disabled', 'disabled');
+            $.post(action, {
+                name: $('#name').val(),
+                title: $('#title').val(),
+                ocupation: $('#ocupation').val(),
+                description: $('#description').val()
+            },
+                function (data) {
+                    document.getElementById('message').innerHTML = data;
+                    $('#message').slideDown('slow');
+                    $('#testimonialform img.loader').fadeOut('slow', function () {
+                        $(this).remove()
+                    });
+                    $('#submit').removeAttr('disabled');
+                    if (data.match('success') != null) $('#testimonialform').slideUp('slow');
+                }
+            );
+        });
+        return false;
+    });
+});
+
 
     /* ==============================================
      CODE WRAPPER -->
